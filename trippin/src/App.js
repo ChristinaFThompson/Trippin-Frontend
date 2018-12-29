f911ed41901d5784aa49f0b05bd6dc95dc32c9ba
“Christina <cfthompson321@gmail.com>
2018-12-29 09:26:50 -0500

    added react-router and landing page

--- trippin/src/App.js
+++ trippin/src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+{
+  /* <Route />  => SETUP ROUTES 
+<Link />  => ALLOWS US TO LINK TO A ROUTE 
+<Switch />  => ALLOWS US TO SWITCH BETWEEN ROUTES, NESTED ROUTES/COMPLEX ROUTES */
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="backgroundImg">
+        <header>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>Trippin'</p>
         </header>
+        {/* <nav>
+        <Link to=""></Link>
+        <Link to=""></Link>
+      </nav>
+      <main>
+        <Route path="" render={}/>
+        {/* // render and component props are two options we can use. */}
+        {/* <Route path="" component={}/>
+      </main> */}
       </div>
     );
   }
